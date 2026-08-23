eeb31ce6291e2e64b68db18f3f469b22da101b5c
satish <ravipallisatish42@gmail.com>
2026-08-23 12:20:23 +0530

    roboshop

--- roboshop1.sh
+++ roboshop1.sh
@@ -18,7 +18,7 @@ do
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi 
-    echo "$instance IP address : $IP"
+    echo "$instance IP address : $IP" #marchant
 
 
 done
