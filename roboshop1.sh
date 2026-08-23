a043a0c568b1605c6fcf1c3921e199b88dc69544
satish <ravipallisatish42@gmail.com>
2026-08-23 15:54:05 +0530

    roboshop

--- roboshop1.sh
+++ roboshop1.sh
@@ -5,7 +5,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-051bd3051b24472e6"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalog" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z06595273AQ0EA2LJG30D"
-DOMAIN_NAME="satishdevops.shop"
+DOMAIN_NAME="satishdevops.shop" 
 
 
 for instance in ${INSTANCES[@]} # for test aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t2.micro --security-group-ids sg-051bd3051b24472e6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]"
@@ -13,12 +13,29 @@ do
 
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t2.micro --security-group-ids sg-051bd3051b24472e6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
-    then 
+    then  # storing ip address 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi 
     echo "$instance IP address : $IP" #marchant
 
-
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+  {
+    "Comment": "Testing creating a record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$instance'.'$DOMAIN_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "' $IP'"
+        }]
+      }
+    }]
+  }
+  
 done
