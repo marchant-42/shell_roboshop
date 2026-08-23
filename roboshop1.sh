4bd011b4768a3fafd2fab5c18ec65de6445d82c5
satish <ravipallisatish42@gmail.com>
2026-08-23 12:11:42 +0530

    roboshop

--- roboshop1.sh
+++ roboshop1.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-echo "satish"
+
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-051bd3051b24472e6"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalog" "user" "cart" "shipping" "payment" "dispatch" "frontend")
