7d29dc0249b223a08d7857a7490560c82027b383
satish <ravipallisatish42@gmail.com>
2026-08-23 12:27:07 +0530

    roboshop

--- roboshop1.sh
+++ roboshop1.sh
@@ -11,7 +11,7 @@ DOMAIN_NAME="satishdevops.shop"
 for instance in ${INSTANCES[@]}
 do 
 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-051bd3051b24472e6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].PrivateIpAddress" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-051bd3051b24472e6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
