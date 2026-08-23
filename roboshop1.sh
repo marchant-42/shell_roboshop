97f2443a2167f58d09a5054dacb4e64486a3aa15
satish <ravipallisatish42@gmail.com>
2026-08-23 10:55:19 +0530

    roboshop

---
+++ roboshop1.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+echo "satish"
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-051bd3051b24472e6"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalog" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z06595273AQ0EA2LJG30D"
+DOMAIN_NAME="satishdevops.shop"
+
+
+for instance in ${INSTANCES[@]}
+do 
